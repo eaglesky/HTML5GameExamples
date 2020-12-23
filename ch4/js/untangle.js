e86f2b8c3ad653ccdd18a9cef8c4fffa397f514e
eaglesky <allenchin1990@gmail.com>
2020-12-23 12:44:23 -0800

    Put the circle drawing code into a function

--- ch4/js/untangle.js
+++ ch4/js/untangle.js
@@ -1,9 +1,11 @@
+if (untangleGame === undefined) {
+    var untangleGame = {};
+}
+
 $(function(){
     var canvas = document.getElementById("game");
-    var ctx = canvas.getContext("2d");
-    ctx.fillStyle = "GOLD";
-    ctx.beginPath();
-    ctx.arc(100, 100, 50, 0, Math.PI*2, true);
-    ctx.closePath();
-    ctx.fill();
+    untangleGame.ctx = canvas.getContext("2d");
+    var width = canvas.width;
+    var height = canvas.height;
+    untangleGame.createRandomCircles(width, height);
 });
