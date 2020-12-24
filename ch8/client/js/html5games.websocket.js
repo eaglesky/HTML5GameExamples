5986e1fa4b14ac4fa21f184a7b154dd71fc08e51
eaglesky <allenchin1990@gmail.com>
2020-12-24 15:53:31 -0800

    Add client

---
+++ ch8/client/js/html5games.websocket.js
@@ -0,0 +1,19 @@
+var websocketGame = {
+}
+
+// init script when the DOM is ready.
+$(function(){
+    // check if existence of WebSockets in browser
+    if (window["WebSocket"]) {
+        // create connection
+        websocketGame.socket = new WebSocket("ws://localhost:8000");
+        // on open event
+        websocketGame.socket.onopen = function(e) {
+            console.log('WebSocket connection established.');
+        };
+        // on close event
+        websocketGame.socket.onclose = function(e) {
+            console.log('WebSocket connection closed.');
+        };
+    }
+});
